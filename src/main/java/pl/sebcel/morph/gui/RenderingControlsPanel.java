fb6e5113f4adea02d294f09f5920370e8424590c
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2018-02-17 15:28:38 +0100

    Removal of 'quality' slider label (should be removed along with the concept of quality)

--- src/main/java/pl/sebcel/morph/gui/RenderingControlsPanel.java
+++ src/main/java/pl/sebcel/morph/gui/RenderingControlsPanel.java
@@ -27,10 +27,9 @@ public class RenderingControlsPanel extends JPanel {
 	public RenderingControlsPanel() {
 		this.setLayout(new GridBagLayout());
 		this.add(new JLabel("Precalculate all frames"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
-		this.add(new JLabel("Quality"), new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
-		this.add(new JLabel("Frame"), new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
+		this.add(new JLabel("Frame"), new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
 		this.add(precalculateButton, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
-		this.add(phaseSlider, new GridBagConstraints(2, 1, 1, 1, 4.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1));
+		this.add(phaseSlider, new GridBagConstraints(1, 1, 1, 1, 4.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1));
 
 		phaseSlider.setMinimum(0);
 		phaseSlider.setMaximum(6);
